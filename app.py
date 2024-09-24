84cbef0faee1f42a6170b9be2ead9a8511011d6a
Mindaugas <darbas.mindaugas@gmail.com>
2024-09-24 21:42:53 +0300

    Adding request lib

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from config import app_config
+from requests import get
 
-print(">>>>")
+res = get("https://delfi.lt")
 
 if app_config.db_user == "":
     raise Exception("FATAL")
-
+else:
+    raise Exception
